50e6cc10622878882666312addcb5ed379a76e8d
Daniel Truong <itkdaniel408@gmail.com>
2023-07-30 17:11:46 -0700

    updated setup script

--- python/setup-py39.sh
+++ python/setup-py39.sh
@@ -4,9 +4,9 @@ sudo apt upgrade
 sudo apt install -y software-properties-common
 add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install -y
-sudo apt install python3.9
-sudo python3.9-distutils
-sudo python3.9-venv
+sudo apt install python3.9 -y
+sudo apt install python3.9-distutils -y
+sudo apt install python3.9-venv -y
 curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
 python3.9 --version
--- sublime/setup-sublime.sh
+++ sublime/setup-sublime.sh
@@ -2,6 +2,6 @@
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install libgl1
-sudo apt-get install libgtk2.0
-sudo apt-get install sublime-text
+sudo apt-get install libgl1 -y
+sudo apt-get install libgtk2.0 -y
+sudo apt-get install sublime-text -y
