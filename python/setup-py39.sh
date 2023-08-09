201fe0d72d17389fc693262cce24ca2dc39c40a1
Daniel Truong <itkdaniel408@gmail.com>
2023-08-08 18:59:16 -0700

    mini update - setup scripts

--- python/setup-py39.sh
+++ python/setup-py39.sh
@@ -2,12 +2,14 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install -y software-properties-common
-add-apt-repository ppa:deadsnakes/ppa -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install -y
 sudo apt install python3.9 -y
 sudo apt install python3.9-distutils -y
 sudo apt install python3.9-venv -y
 curl -sSL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3.9 get-pip.py
+sudo apt install python3-pip -y
+sudo ln -sf /usr/bin/python3.9 /usr/bin/python3
 python3.9 --version
 pip3 --version
